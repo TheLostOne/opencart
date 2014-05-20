b02a36d606d3f634e81f0a75b347518a8c84981e
TheLostOne <tesconda2@hotmail.com>
2014-05-20 10:00:47 +0100

    Update category.php
    Improved Categories Menu;
    a) 1 DB call for All Categories (rather than 1 for root cats + 1 per cat for children).
    b) 1 DB call for Product Counts (rather than 1 per category).
    c) Unlimited levels (no more Parent+Child only!).
    
    ToDo;
    1) will introduce a MaxLevel option
    2) will refactor so ProdCount is standard for NoStubs - but visible output is optional (hide 0's as well!)

--- upload/catalog/controller/module/category.php
+++ upload/catalog/controller/module/category.php
@@ -1,9 +1,89 @@
 <?php
 class ControllerModuleCategory extends Controller {
-	public function index($setting) {
-		$this->load->language('module/category');
 
-		$data['heading_title'] = $this->language->get('heading_title');
+	public function buildTree(array $elements, $counts, $parentId, $chain) {
+	// Takes AllCategories and Counts arrays, builds a Tree structure
+		
+    	$branch = array();
+		$getcounts = $this->config->get('config_product_count');
+		
+    	foreach ($elements as $key=>$element) {
+        	if ($element['parent_id'] == $parentId) {
+
+				// >> Chain + URL-Path : Cat path (id_id_id etc.)
+				if(empty($chain) || !in_array($element['parent_id'], $chain)){
+					if($parentId == 0){
+						$chain[]= '';
+					}else{
+						$chain[]= $element['parent_id'];
+					}
+				}
+				$element['chain'] = $chain;
+				$urlpath = (implode('_',$chain)).'_'.$element['category_id'];
+				$urlpath = ltrim($urlpath,'_');
+
+				
+				// >> Product Counts
+				$element['thistotal'] = '0';
+
+				// If using Counts, and we have them!
+				if($getcounts && is_array($counts) && !empty($counts)){
+					
+					// If the CatID is in the Count array
+					if(array_key_exists($element['category_id'], $counts)){
+						$element['thistotal'] = $counts[$element['category_id']];
+					}else{
+						// Not in the array, thus no products in that category
+						$element['thistotal'] = '0';
+					}
+				}
+				
+				$element['href'] = $this->url->link('product/category', 'path=' . $urlpath);
+				$element['name'] = $element['name'] . ' [ ' .$element['thistotal']  .' ]';
+				
+				$children = $this->buildTree($elements, $counts, $element['category_id'], $element['chain']);
+            	if ($children) {
+	                $element['children'] = $children;
+					$chain = '';
+        	    }
+
+				
+            	$branch[$element['category_id']] = $element;
+				//$this->pruneTree($branch);
+			
+        	}
+    	}
+		
+    	return $branch;
+	}	
+	// END FUNC
+
+
+	public function pruneTree(&$nodes) {
+		// Func removes Categories from category tree if
+		// it has no children AND no products.
+		// It will leave Categories with no products if it has a child,
+		// (or grandchild etc.), that has a product
+		
+    	foreach ($nodes as $key => $node) {
+        	if (!isset($node['children']) && $node['thistotal'] == 0) {
+            	unset($nodes[$key]);
+	        } elseif (!empty($node['children'])) {
+    	        $this->pruneTree($nodes[$key]['children']);
+        	    // This line checks if all the children have been pruned away:
+            	if (empty($nodes[$key]['children']) && $nodes[$key]['thistotal'] == 0) {
+                	unset($nodes[$key]);
+	            }
+    	    }
+	    }
+	}
+	// END FUNC
+
+	protected function index($setting) {
+
+		$this->language->load('module/category');
+
+		$this->data['heading_title'] = $this->language->get('heading_title');
 
 		if (isset($this->request->get['path'])) {
 			$parts = explode('_', (string)$this->request->get['path']);
@@ -12,60 +92,57 @@ class ControllerModuleCategory extends Controller {
 		}
 
 		if (isset($parts[0])) {
-			$data['category_id'] = $parts[0];
+			$this->data['category_id'] = $parts[0];
 		} else {
-			$data['category_id'] = 0;
+			$this->data['category_id'] = 0;
 		}
 
 		if (isset($parts[1])) {
-			$data['child_id'] = $parts[1];
+			$this->data['child_id'] = $parts[1];
 		} else {
-			$data['child_id'] = 0;
+			$this->data['child_id'] = 0;
 		}
 
 		$this->load->model('catalog/category');
 
 		$this->load->model('catalog/product');
 
-		$data['categories'] = array();
+		$this->data['categories'] = array();
 
-		$categories = $this->model_catalog_category->getCategories(0);
 
-		foreach ($categories as $category) {
-			$children_data = array();
+		
+		$categories = $this->model_catalog_category->getAllCategories();
 
-			$children = $this->model_catalog_category->getCategories($category['category_id']);
-
-			foreach ($children as $child) {
-				$filter_data = array(
-					'filter_category_id'  => $child['category_id'],
-					'filter_sub_category' => true
-				);
-
-				$children_data[] = array(
-					'category_id' => $child['category_id'],
-					'name'        => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-					'href'        => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-				);
-			}
-
-			$filter_data = array(
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			);
-
-			$data['categories'][] = array(
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-				'children'    => $children_data,
-				'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
-			);
+		
+		$productcounts = $this->model_catalog_product->getAllTotalProducts();
+		$productcounts2 = '';
+		foreach($productcounts as $proddata){
+			$productcounts2[$proddata['category_id']] = $proddata['COUNT(*)'];
 		}
+		unset($productcounts);
+
+
+
+		$chain = array();
+
+		$category_tree_data = $this->buildTree($categories, $productcounts2, 0, $chain); 
+		
+		$nostubs = true;
+		if($this->config->get('config_product_count') && $nostubs){
+			$this->pruneTree($category_tree_data);
+		}
+		
+		 
+		$this->data['categories'] = $category_tree_data;
+
 
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/category.tpl')) {
-			return $this->load->view($this->config->get('config_template') . '/template/module/category.tpl', $data);
+			$this->template = $this->config->get('config_template') . '/template/module/category.tpl';
 		} else {
-			return $this->load->view('default/template/module/category.tpl', $data);
+			$this->template = 'default/template/module/category.tpl';
 		}
+
+		$this->render();
 	}
 }
+?>
