2660b16c90cd45f797ef542d68c9de02f9bb20de
TheLostOne <tesconda2@hotmail.com>
2014-05-20 09:22:59 +0100

    Update pagination.php
    Update includes 2 minor code changes to fix non-canonical "page 1" URLs.
    Now any pager-link to the "first page" or "page 1" will point to a clean/canonical URL.

--- upload/system/library/pagination.php
+++ upload/system/library/pagination.php
@@ -3,37 +3,54 @@ class Pagination {
 	public $total = 0;
 	public $page = 1;
 	public $limit = 20;
-	public $num_links = 8;
+	public $num_links = 10;
 	public $url = '';
+	public $text = 'Showing {start} to {end} of {total} ({pages} Pages)';
 	public $text_first = '|&lt;';
 	public $text_last = '&gt;|';
 	public $text_next = '&gt;';
 	public $text_prev = '&lt;';
+	public $style_links = 'links';
+	public $style_results = 'results';
 
 	public function render() {
 		$total = $this->total;
-
+		
 		if ($this->page < 1) {
 			$page = 1;
 		} else {
 			$page = $this->page;
 		}
-
+		
 		if (!(int)$this->limit) {
 			$limit = 10;
 		} else {
 			$limit = $this->limit;
 		}
-
+		
 		$num_links = $this->num_links;
 		$num_pages = ceil($total / $limit);
-
-		$output = '<ul class="pagination">';
-
+		
+		$output = '';
+		
+		/*
 		if ($page > 1) {
-			$output .= '<li><a href="' . str_replace('{page}', 1, $this->url) . '">' . $this->text_first . '</a></li>';
-			$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a></li>';
+			$output .= ' <a href="' . str_replace('{page}', 1, $this->url) . '">' . $this->text_first . '</a> <a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a> ';
 		}
+		*/
+		// Canonical Page 1 Pager fix (1 of 2)
+		// Replaced above with the below
+		if ($page > 1) {
+		    if($page == 2){
+        		$firsturl = str_replace('&page={page}', '', $this->url);
+		        $previousurl =  str_replace('&page={page}', '', $this->url);
+		    } else {
+        		$firsturl = str_replace('&page={page}', '', $this->url);
+		        $previousurl =  str_replace('{page}', $page - 1, $this->url);
+		    }
+		    $output .= ' <a href="' . $firsturl . '">' . $this->text_first . '</a> <a href="' . $previousurl . '">' . $this->text_prev . '</a> ';
+		}
+
 
 		if ($num_pages > 1) {
 			if ($num_pages <= $num_links) {
@@ -42,38 +59,69 @@ class Pagination {
 			} else {
 				$start = $page - floor($num_links / 2);
 				$end = $page + floor($num_links / 2);
-
+			
 				if ($start < 1) {
 					$end += abs($start) + 1;
 					$start = 1;
 				}
-
+						
 				if ($end > $num_pages) {
 					$start -= ($end - $num_pages);
 					$end = $num_pages;
 				}
 			}
 
+			if ($start > 1) {
+				$output .= ' .... ';
+			}
+
+			/*
 			for ($i = $start; $i <= $end; $i++) {
 				if ($page == $i) {
-					$output .= '<li class="active"><span>' . $i . '</span></li>';
+					$output .= ' <b>' . $i . '</b> ';
 				} else {
-					$output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
-				}
+					$output .= ' <a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a> ';
+				}	
+			}
+			*/
+			// Canonical Page 1 Pager fix (2 of 2)
+			// Replaced above with the below		
+			for ($i = $start; $i <= $end; $i++) {
+    			if ($page == $i) {
+			        $output .= ' <b>' . $i . '</b> ';
+			    } elseif($i == 1) {
+			        $output .= ' <a href="' . str_replace('&page={page}', '', $this->url) . '">' . $i . '</a> ';
+			    } else {
+			        $output .= ' <a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a> ';
+			    }   
+			}
+
+					
+			if ($end < $num_pages) {
+				$output .= ' .... ';
 			}
 		}
-
+		
 		if ($page < $num_pages) {
-			$output .= '<li><a href="' . str_replace('{page}', $page + 1, $this->url) . '">' . $this->text_next . '</a>';
-			$output .= '<li><a href="' . str_replace('{page}', $num_pages, $this->url) . '">' . $this->text_last . '</a>';
+			$output .= ' <a href="' . str_replace('{page}', $page + 1, $this->url) . '">' . $this->text_next . '</a> <a href="' . str_replace('{page}', $num_pages, $this->url) . '">' . $this->text_last . '</a> ';
 		}
-
-		$output .= '</ul>';
-
-		if ($num_pages > 1) {
-			return $output;
-		} else {
-			return '';
-		}
+		
+		$find = array(
+			'{start}',
+			'{end}',
+			'{total}',
+			'{pages}'
+		);
+		
+		$replace = array(
+			($total) ? (($page - 1) * $limit) + 1 : 0,
+			((($page - 1) * $limit) > ($total - $limit)) ? $total : ((($page - 1) * $limit) + $limit),
+			$total, 
+			$num_pages
+		);
+		
+		
+		return ($output ? '<div class="' . $this->style_links . '">' . $output . '</div>' : '') . '<div class="' . $this->style_results . '">' . str_replace($find, $replace, $this->text) . '</div>';
 	}
 }
+?>
